9d9d54d45985bc6d4f25fb1eb475f4c5262ca311
Dominik Strasser <dominik.strasser@netural.com>
2017-03-16 11:07:24 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,132 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+
+import { Observable } from 'rxjs';
+declare var FB;
+
+interface REACTION_TYPES {
+
+  LIKE: number,
+  LOVE: number,
+  HAHA: number,
+  WOW: number,
+  SAD: number,
+  ANGRY: number
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Facebook Live Voting';
+
+  public isLoggedIn = false;
+  public user;
+  public reactions: REACTION_TYPES;
+
+
+  /**
+   *
+   * TODO: UPDATE THE VARIABLES!
+   */
+
+  private VARIABLES = {
+    USER_ID: '',
+    POST_ID: ''
+  }
+
+  constructor(
+    private cdRef: ChangeDetectorRef
+  ) {
+    console.log('INIT');
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.checkLoginStatus();
+      this.resetReactions();
+      this.initPolling();
+    }, 3000);
+  }
+
+  resetReactions() {
+    this.reactions = {
+      LIKE: 0,
+      LOVE: 0,
+      HAHA: 0,
+      WOW: 0,
+      SAD: 0,
+      ANGRY: 0
+    };
+  }
+
+  initPolling() {
+    Observable.interval(4000).startWith(0).subscribe(() => {
+      if (!this.isLoggedIn) return;
+      this.getReactions();
+    });
+  }
+
+  checkLoginStatus() {
+    FB.getLoginStatus((response) => {
+      if (response.status === 'connected') {
+        console.log('Logged in.');
+        this.getUserInfo();
+        this.isLoggedIn = true;
+      } else {
+        console.log('Logged out.');
+        this.isLoggedIn = false;
+      }
+    });
+  }
+
+  login() {
+    FB.login((response) => {
+      if (response.authResponse) {
+        this.getUserInfo();
+        this.isLoggedIn = true;
+      } else {
+        console.log('Authorization failed.');
+      }
+    }, { scope: 'email, user_posts' });
+  }
+
+
+  getUserInfo() {
+
+    FB.api('/me', (response) => {
+      this.user = response;
+      this.cdRef.detectChanges();
+      console.log(response);
+    });
+
+  }
+
+  getReactions() {
+    console.log('get reactions');
+
+    FB.api(
+      `/${this.VARIABLES.USER_ID}_${this.VARIABLES.POST_ID}/reactions`,
+      (response) => {
+        console.log(response);
+        if (response && !response.error) {
+          this.resetReactions();
+          for (let i = 0; i < response.data.length; i++) {
+            const reaction = response.data[i];
+            this.reactions[reaction.type] += 1;
+          }
+        }
+      }
+    );
+  }
+
+  getReactionsKeys() {
+    if (this.reactions) {
+      return Object.keys(this.reactions)
+    }
+  }
+
+
 }
